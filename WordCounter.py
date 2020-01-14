06ffdf1248d7bebe5720781a8569bd84bc8b8a4d
Hoa Pham <rzz934@my.utsa.edu>
2020-01-14 14:53:23 -0600

    challenge answer

---
+++ WordCounter.py
@@ -0,0 +1,83 @@
+import sys
+import os
+import re
+import collections
+from collections import defaultdict
+
+class WordCounter():
+
+    def __init__(self, inputFileName):
+        self.wordCountDict, self.longestWordLength = self.generateWordCount(inputFileName)
+
+    def generateHistogram(self):
+        """
+        Generate a output of the word count of each word in wordCountDict.
+        The output consists of the word followed by a pipe character ("|"), 
+        a number of equal signs that are proportional to the number of 
+        occurrences found in the text, and the number of occurrences itself.
+
+        Outputs
+        -------
+            output.txt file that contains the word counts from wordCountDict.
+        """
+        outputFileName = 'output.txt'
+        with open(outputFileName, 'w') as outputFile:
+            for word in sorted(self.wordCountDict, key=self.wordCountDict.get, reverse=True):
+                indentSpaces = self.longestWordLength - len(word)
+                print(f"{indentSpaces*' '}{word} | {self.wordCountDict[word]*'='} ({self.wordCountDict[word]})", file = outputFile)
+
+    def generateWordCount(self, inputFileName):
+        """
+        - Opens the file with given name and populates a dictionary which contains the 
+        counts of number of occurences of each word from the input file. 
+        - Stores the length of the longest word from the input file.
+
+        Parameters
+        ----------
+        inputFileName: str
+            name of input file
+
+        Returns
+        -------
+        dict
+            A dictionary that stores the count of occurences of each word from
+            the input file.
+        int
+            Length of the longest word from the input file.
+        """
+        wordCountDict = defaultdict(int)
+        longestWordLength = -1
+
+        with open(inputFileName, 'r') as inputFile:
+            while True:
+                inputLine = inputFile.readline()
+
+                if inputLine == "": # EOF
+                    break
+
+                inputLine = inputLine.rstrip('\n')	# get rid of new line character of 
+										            # original each line
+                line = inputLine.lstrip()			# get rid of leading whitespaces of each
+                                                    # stripped input line
+
+                # if the is empty, go to the next one
+                if line == '':
+                    continue
+
+                # split the line into tokens
+                tokens = line.split(' ')
+
+                # process each token 
+                for token in tokens:
+                    # filter out special characters in the token
+                    word = self.filterSpecialCharacters(token)
+                    # check if word has longer length than current longestWordLength
+                    longestWordLength = max(len(word), longestWordLength)
+                    # add the word count 
+                    wordCountDict[word] += 1 
+
+        return wordCountDict, longestWordLength
+
+    def filterSpecialCharacters(self, token):
+        # filter out special characters and transform it into lowercase word and return the result
+        return re.sub('\W+', '', token).lower()
---
+++ wordCounterDriver.py
@@ -0,0 +1,57 @@
+"""
+- This program reads in a paragraph from an input file and generates a histogram
+of the words used sorted from most occurrences to least. The output will consist 
+of the word followed by a pipe character ("|"), a number of equal signs that are 
+proportional to the number of occurrences found in the text, and the number of 
+occurrences itself. The generated histogram will be stored in an output file (output.txt)
+- Sample Input:
+    Hickory, dickory, dock.
+    The mouse ran up the clock. The clock struck one,
+    The mouse ran down, Hickory, dickory, dock.
+- Sample Output:
+        the | ==== (4)
+    hickory | == (2)
+    dickory | == (2)
+       dock | == (2)
+      mouse | == (2)
+        ran | == (2)
+      clock | == (2)
+         up | = (1)
+     struck | = (1)
+        one | = (1)
+       down | = (1)
+
+Command Parameters
+------------------
+	python3 wordCounterDriver.py inputFile
+
+Results
+-------
+	- All output is written to output file (output.txt).
+
+Notes
+-----
+    - The program ignores case sensitivity from the input file
+    - All special characters from the input file will be filtered out.
+	- This program will call functions from WordCounter.py
+"""
+
+import sys
+import os
+from WordCounter import WordCounter
+
+# validate number of input arguments
+if len(sys.argv) < 2:
+    print("filename needed as command argument")
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+# validate input file
+if os.path.isfile(filename) == False:
+    print(f"Error: {filename} is not a file.")
+    print("Exit Program")
+    sys.exit(1)
+
+wc = WordCounter(filename)
+wc.generateHistogram()
